69ca26a1bc5764682b7c9bb9fc039ab073c98b4d
Махмудов Мухаммаджон Абдумаджидович <MAMakhmudov@sberbank.ru>
2023-07-09 15:39:42 +0300

    shri: pushing

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import {Header} from "./components/Header/Header.jsx";
+import {Main} from "./components/Main/Main";
+
+function App() {
+
+  return <>
+    <Header />
+    <Main />
+  </>
+}
+
+export default App
